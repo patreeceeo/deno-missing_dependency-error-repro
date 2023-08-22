f77343391087dec0bbb6c00fbfab77745f4a7c53
Patrick Canfield <pscale01@gmail.com>
2023-08-21 19:05:26 -0700

    min repro

---
+++ public/index.mjs
@@ -0,0 +1,14 @@
+// src/Constants.js
+/// <reference types="./index.d.ts" />
+var TYPES_ENUM = {
+  i8: "i8",
+  ui8: "ui8",
+  ui8c: "ui8c",
+  i16: "i16",
+  ui16: "ui16",
+  i32: "i32",
+  ui32: "ui32",
+  f32: "f32",
+  f64: "f64",
+  eid: "eid"
+};
---
+++ src/test.ts
@@ -0,0 +1,2 @@
+import {TYPES_ENUM as Types} from 'bitecs';
+console.log(Types);
